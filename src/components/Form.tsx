47dd7b3f2b0b88eab1f853d9af2447f321f8daa0
yagmurcetintas <yagmurcetin@gmail.com>
2021-01-22 14:57:10 -0500

    Added basic form elements and basic styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,57 +1,168 @@
-import type { FunctionComponent } from "react";
-import { useRef } from "react";
+import type { FormEvent, FunctionComponent } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Mascot from "../images/IMG_0405.png";
 import styles from "./Form.module.css";
+import Layout from "./Layout";
 
 const Form: FunctionComponent = () => {
   const inputEl = useRef<HTMLInputElement>(null);
+  const [dropdownBtnActive, setDropdownBtnActive] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState("Cash");
 
-  const handleSubmit = (): void => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     // strict null checks need us to check if inputEl and current exist.
     // but once current exists, it is of type HTMLInputElement, thus it
     // has the method focus! ✅
-    console.log(inputEl.current);
+    e.preventDefault();
+    console.log(inputEl.current?.value);
+  };
+
+  const handleDropdownClick = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    dropdownBtnActive ? setDropdownBtnActive(false) : setDropdownBtnActive(true);
+    e.stopPropagation();
+  };
+
+  const closeDropdown = useCallback((): void => {
+    setDropdownBtnActive(false);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("click", closeDropdown);
+    return (): void => {
+      window.removeEventListener("click", closeDropdown);
+    };
+  }, [closeDropdown]);
+
+  const choosePayment = (payment: string): void => {
+    setPaymentMethod(payment);
+  };
+
+  const renderCashJSX = (): JSX.Element => {
+    return (
+      <div>
+        <div className={styles.inputField}>
+          <label htmlFor="salesPrice">Sales Price:</label>
+          <input id="salesPrice" name="salesPrice" ref={inputEl} type="number" />
+          <span className={styles.unit}>$</span>
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="salesTax">Sales Tax:</label>
+          <input id="salesTax" name="salesTax" type="number" />
+          <span className={styles.unit}>%</span>
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="upfrontCosts">Upfront Costs:</label>
+          <input id="upfrontCosts" name="upfrontCosts" type="number" />
+          <span className={styles.unit}>$</span>
+        </div>
+      </div>
+    );
+  };
+
+  const renderLoanJSX = (): JSX.Element => {
+    return (
+      <div>
+        <div className={styles.inputField}>
+          <label htmlFor="loanInterest">Loan Interest:</label>
+          <input id="loanInterest" name="loanInterest" ref={inputEl} type="number" />
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="loanDuration">Loan Duration:</label>
+          <input id="loanDuration" name="loanDuration" type="number" />
+        </div>
+      </div>
+    );
   };
 
   return (
-    <form>
-      <div className={styles.mainContainer}>
-        <div>
-          <div className={styles.field}>
-            <label htmlFor="name">Name</label>
-            <input id="name" name="name" ref={inputEl} type="text" />
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="score">Score</label>
-            <input id="score" name="score" type="number" />
-          </div>
-        </div>
+    <Layout>
+      <div className={styles.banner}>
         <img alt="mascot" className={styles.img} src={Mascot} />
-        <div>
-          <div className={styles.field}>
-            <label htmlFor="name">Name</label>
-            <input id="name" name="name" type="text" />
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="score">Score</label>
-            <input id="score" name="score" type="number" />
-          </div>
-        </div>
-        <div>
-          <button onSubmit={handleSubmit} type="submit">
-            Submit
-          </button>
-        </div>
+        <p>Want to know which one makes more sense for you?</p>
       </div>
-    </form>
+      <form className={styles.mainContainer} onSubmit={handleSubmit}>
+        <div className={styles.formContainer}>
+          <p className={styles.mainHeader}>BUY</p>
+          <div className={styles.dropdown}>
+            <button className={styles.dropbtn} onClick={handleDropdownClick} type="button">
+              <span className={styles.title}>Payment Method: </span>
+              <span>
+                {paymentMethod}
+                <span className={styles.dropIcon}> ▼</span>
+              </span>
+              <div
+                className={
+                  dropdownBtnActive ? styles.dropdownContentActive : styles.dropdownContent
+                }
+              >
+                <p
+                  className={paymentMethod === "Cash" ? styles.active : ""}
+                  onClick={(): void => {
+                    choosePayment("Cash");
+                  }}
+                >
+                  Cash
+                </p>
+                <p
+                  className={paymentMethod === "Loan" ? styles.active : ""}
+                  onClick={(): void => {
+                    choosePayment("Loan");
+                  }}
+                >
+                  Loan
+                </p>
+              </div>
+            </button>
+            {paymentMethod === "Cash" ? renderCashJSX() : renderLoanJSX()}
+          </div>
+        </div>
+        <div>
+          <div className={styles.formContainer}>
+            <p className={styles.mainHeader}>LEASE</p>
+            <div className={styles.inputField}>
+              <label htmlFor="leasePrice">Monthly Lease Price:</label>
+              <input id="leasePrice" name="leasePrice" type="number" />{" "}
+              <span className={styles.unit}>$</span>
+            </div>
+            <div className={styles.inputField}>
+              <label htmlFor="salesTax">Sales Tax:</label>
+              <input id="salesTax" name="salesTax" type="number" />{" "}
+              <span className={styles.unit}>%</span>
+            </div>
+            <div className={styles.inputField}>
+              <label htmlFor="leaseDuration">Lease Duration:</label>
+              <input id="leaseDuration" name="leaseDuration" type="number" />{" "}
+              <span className={styles.unit}>mo</span>
+            </div>
+            <div className={styles.buttonAndFormWrapper}></div>
+            <div className={styles.inputField}>
+              <label htmlFor="residualPrice">Residual Price:</label>
+              <input id="residualPrice" name="residualPrice" type="number" />{" "}
+              <span className={styles.unit}>$</span>
+            </div>
+            <div className={styles.inputField}>
+              <label htmlFor="investmentReturn">Expected Yearly Investment Return:</label>
+              <input id="investmentReturn" name="investmentReturn" type="number" />{" "}
+              <span className={styles.unit}>$</span>
+            </div>
+            <div className={styles.wrapper}>Upfront Costs</div>
+            <div className={styles.inputField}>
+              <label htmlFor="upfrontPayment">Upfront Payment:</label>
+              <input id="upfrontPayment" name="upfrontPayment" type="number" />
+              <span className={styles.unit}>$</span>
+            </div>
+            <div className={styles.inputField}>
+              <label htmlFor="taxesAndFees">Taxes & Fees:</label>
+              <input id="taxesAndFees" name="taxesAndFees" type="number" />
+              <span className={styles.unit}>$</span>
+            </div>
+          </div>
+          <div>
+            <button type="submit">Calculate</button>
+          </div>
+        </div>
+      </form>
+    </Layout>
   );
 };
 
