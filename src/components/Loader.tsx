56a424b289418cc4bc106bb376055bd36208f55e
yagmurcetintas <yagmurcetin@gmail.com>
2021-04-30 16:24:09 -0400

    Added loader styling

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 import { useEffect, useState } from "react";
+import Logo from "../images/monke.png";
+import styles from "./Loader.module.css";
 
 const textArr: string[] = [
   "Monkes are working",
@@ -21,7 +23,12 @@ const Loader: FC = () => {
     };
   }, []);
 
-  return <div>{textArr[textIndex]}</div>;
+  return (
+    <div className={styles.mainLoaderContainer}>
+      <img alt="monke logo" className={styles.monkeyIcon} src={Logo} />
+      <p className={styles.text}>{textArr[textIndex]}</p>
+    </div>
+  );
 };
 
 export default Loader;
